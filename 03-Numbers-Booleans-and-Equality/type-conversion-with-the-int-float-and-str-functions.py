f06610ed1c890d6cf9e51faea27de048ad703154
sambowden94 <sambowden@live.com>
2024-09-18 07:39:47 +0000

    Lesson 7

---
+++ 03-Numbers-Booleans-and-Equality/type-conversion-with-the-int-float-and-str-functions.py
@@ -0,0 +1,20 @@
+print(int(3.14))
+print(int(3.99))
+print(int("3"))
+print(int(3))
+print(float(5))
+print(float("10.32"))
+print(float(5.23))
+
+print(str(5.35))
+print(str(5))
+print(str("hello"))
+
+print(str(5) + "hello")
+
+print(3 + 3)
+print(4.3 + 5.7)
+print(3 + 5.8)
+
+print(int(float("3.14")))
+
---
+++ 04-Variables/intro-to-variables.py
@@ -0,0 +1,24 @@
+name = "Sam"
+age = 30
+handsome = True
+favourite_language_at_the_moment = "Python"
+print(name)
+print(handsome)
+
+print(age + 4)
+
+print("My name is", name, "and I am", age, "years old")
+
+age = 39
+print(age)
+
+age = 20 + 10
+print(age)
+
+fact_or_fiction = 6 < 10
+print(fact_or_fiction)
+
+chameleon = 5
+print(Chameleon)
+
+chameleon = 5
